d8b4e86f235430b77d24e6310fe76095ad04cf7c
Nicomotac <mota.nicolas@gmail.com>
2025-10-04 20:59:59 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import { Outlet } from "react-router-dom";
+import Menu from "./Components/Menu";
+import Rodape from "./Components/Rodape";
 
 export default function App() {
 
 return(
-
-
-<Outlet/>
-
-
-
+  <>
+	<Menu/>
+	<Outlet/>
+	<Rodape/>
+  </>
 )
-
-
-
 
 }
